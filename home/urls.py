d094a653d81c6ca09cf8e76ca85723ccc71d5247
Eduardoedy17 <eduardoedy1717@gmail.com>
2026-01-03 18:03:32 -0300

    ..

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('categoria/', views.categoria, name="categoria"),
     path('categoria/form/', views.form_categoria, name="form_categoria"),
+    path('categoria/editar/<int:id>/', views.editar_categoria, name="editar_categoria"),
+    path('categoria/excluir/<int:id>/', views.excluir_categoria, name="excluir_categoria"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import redirect, render
-
-
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import *
 from .forms import *
 
@@ -25,3 +23,22 @@ def form_categoria(request):
         'form':form,
     }
     return render(request, 'categoria/formulario.html', contexto)
+
+def editar_categoria(request, id):
+    categoria = get_object_or_404(Categoria, pk=id)
+    if request.method == 'POST':
+        form = CategoriaForm(request.POST, instance=categoria)
+        if form.is_valid():
+            form.save()
+            return redirect('categoria')
+    else:
+        form = CategoriaForm(instance=categoria)
+    contexto = {
+        'form': form,
+    }
+    return render(request, 'categoria/formulario.html', contexto)
+
+def excluir_categoria(request, id):
+    categoria = get_object_or_404(Categoria, pk=id)
+    categoria.delete()
+    return redirect('categoria')
