d35430ad0df67f19c0ac66dc890035751ada7b67
Eduardoedy17 <eduardoedy1717@gmail.com>
2026-01-12 20:54:12 -0300

    ...

--- home/forms.py
+++ home/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from .models import *
 
-
 class CategoriaForm(forms.ModelForm):
     class Meta:
         model = Categoria
@@ -10,3 +9,16 @@ class CategoriaForm(forms.ModelForm):
             'nome':forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome'}),
             'ordem':forms.NumberInput(attrs={'class': 'inteiro form-control', 'placeholder': ''}),
         }
+
+        def clean_nome(self):
+            nome = self.cleaned_data.get('nome')
+            if len(nome) < 3:
+                raise forms.ValidationError("O nome deve ter pelo menos 3 caracteres.")
+            return nome  
+    
+        def clean_ordem(self):
+            ordem = self.cleaned_data.get('ordem')
+            if ordem <= 0:
+                raise forms.ValidationError("O campo ordem deve ser maior que zero.")
+            return ordem
+
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib import messages
 from .models import *
 from .forms import *
 
@@ -15,7 +16,8 @@ def form_categoria(request):
     if request.method == 'POST':
        form = CategoriaForm(request.POST) # instancia o modelo com os dados do form
        if form.is_valid():# faz a validação do formulário
-            form.save() # salva a instancia do modelo no banco de dados
+            categoria = form.save() # salva a instancia do modelo no banco de dados
+            messages.success(request, 'Registro salvo com sucesso!')
             return redirect('categoria') # redireciona para a listagem
     else:# método é get, novo registro
         form = CategoriaForm() # formulário vazio
@@ -24,21 +26,45 @@ def form_categoria(request):
     }
     return render(request, 'categoria/formulario.html', contexto)
 
-def editar_categoria(request, id):
+def detalhes_categoria(request, id):
     categoria = get_object_or_404(Categoria, pk=id)
-    if request.method == 'POST':
-        form = CategoriaForm(request.POST, instance=categoria)
-        if form.is_valid():
-            form.save()
-            return redirect('categoria')
-    else:
-        form = CategoriaForm(instance=categoria)
+    
+    # Instancia o formulário com o objeto
+    form = CategoriaForm(instance=categoria)
+    
+    # Bloqueia todos os campos
+    for field in form.fields.values():
+        field.widget.attrs['disabled'] = 'disabled'
+
     contexto = {
         'form': form,
+        'titulo': 'Detalhes da Categoria',  # Título personalizado
+        'apenas_leitura': True  # Flag para esconder o botão Salvar
     }
     return render(request, 'categoria/formulario.html', contexto)
+
+def editar_categoria(request, id):
+    try:
+        categoria = Categoria.objects.get(pk=id)
+    except Categoria.DoesNotExist:
+        # Caso o registro não seja encontrado, exibe a mensagem de erro
+        messages.error(request, 'Registro não encontrado')
+        return redirect('categoria')  # Redireciona para a listagem
+     
+    if request.method == 'POST':
+        # combina os dados do formulário submetido com a instância do objeto existente, permitindo editar seus valores.
+        form = CategoriaForm(request.POST, instance=categoria)
+        if form.is_valid():
+            categoria = form.save() # save retorna o objeto salvo
+            messages.success(request, 'Operação realizada com Sucesso')
+            return redirect('categoria') # redireciona para a listagem
+    else:
+         form = CategoriaForm(instance=categoria)
+    return render(request, 'categoria/formulario.html', {'form': form,})
+
 
 def excluir_categoria(request, id):
     categoria = get_object_or_404(Categoria, pk=id)
     categoria.delete()
+    messages.success(request, 'Registro excluído com sucesso!')
     return redirect('categoria')
